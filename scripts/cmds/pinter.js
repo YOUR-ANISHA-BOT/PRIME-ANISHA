0ca2be02c60c3517c53d6e6e89b944ad8a29e11c
MR-TARIF-BOT-X404 <mr.arafat.x404@gmail.com>
2025-10-03 18:41:54 +0600

    Create pinter.js

---
+++ scripts/cmds/pinter.js
@@ -0,0 +1,91 @@
+const axios = require("axios");
+const fs = require("fs-extra");
+const path = require("path");
+const baseApiUrl = async () => {
+    const base = await axios.get(
+        `https://raw.githubusercontent.com/Blankid018/D1PT0/main/baseApiUrl.json`,
+    );
+    return base.data.api;
+};
+
+module.exports = {
+    config: {
+        name: "pinter",
+        aliases: ["pin"],
+        version: "1.0",
+        author: "Dipto",
+        countDown: 15,
+        role: 0,
+        shortDescription: "Pinterest Image Search",
+        longDescription: "Pinterest Image Search",
+        category: "IMAGE",
+        guide: {
+            en: "{pn} pin nam -?",
+        },
+    },
+
+    onStart: async function ({ api, event, args }) {
+        const queryAndLength = args.join(" ").split("-");
+        const q = queryAndLength[0].trim();
+        const length = queryAndLength[1].trim();
+
+        if (!q || !length) {
+            return api.sendMessage(
+                "📛| Wrong Format",
+                event.threadID,
+                event.messageID,
+            );
+        }
+
+        try {
+            const w = await api.sendMessage("𝐏𝐥𝐳 𝐰8......🔎", event.threadID);
+            const response = await axios.get(
+                `${await baseApiUrl()}/pinterest?search=${encodeURIComponent(q)}&limit=${encodeURIComponent(length)}`,
+            );
+            const data = response.data.data;
+
+            if (!data || data.length === 0) {
+                return api.sendMessage(
+                    "Empty response or no images found.",
+                    event.threadID,
+                    event.messageID,
+                );
+            }
+
+            const diptoo = [];
+            const totalImagesCount = Math.min(data.length, parseInt(length));
+
+            for (let i = 0; i < totalImagesCount; i++) {
+                const imgUrl = data[i];
+                const imgResponse = await axios.get(imgUrl, {
+                    responseType: "arraybuffer",
+                });
+                const imgPath = path.join(
+                    __dirname,
+                    "dvassests",
+                    `${i + 1}.jpg`,
+                );
+                await fs.outputFile(imgPath, imgResponse.data);
+                diptoo.push(fs.createReadStream(imgPath));
+            }
+
+            await api.unsendMessage(w.messageID);
+            await api.sendMessage(
+                {
+                    body: `
+🔰 | 𝐇𝐞𝐫𝐞'𝐬 𝐘𝐨𝐮𝐫 𝐐𝐮𝐞𝐫𝐲 𝐁𝐚𝐬𝐞𝐝 𝐢𝐦𝐚𝐠𝐞𝐬\n📜 | 𝐓𝐨𝐭𝐚𝐥 𝐈𝐦𝐚𝐠𝐞𝐬 𝐂𝐨𝐮𝐧𝐭: ${totalImagesCount}`,
+                    attachment: diptoo,
+                },
+                event.threadID,
+                event.messageID,
+            );
+        } catch (error) {
+            console.error(error);
+            await api.sendMessage(
+                `Error: ${error.message}`,
+                event.threadID,
+                event.messageID,
+            );
+        }
+    },
+};
